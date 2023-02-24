09abf054866f68e892685f619d9d0cd9e0f66c32
KendraAnderson <kor04003@byui.edu>
2023-02-24 13:38:06 -0800

    lesson1 finish rearranged

--- server.js
+++ server.js
@@ -1,24 +1,9 @@
 const express = require('express');
 const app = express();
-const mongodb = require('./db/connect');
-const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
-const MongoClient = require('mongodb').MongoClient;
-const routes = require('./routes/index');
 
-app.use(bodyParser.json())
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-    })
-    .use('/', require('./routes'));
+app.use('/', require('./routes'));
 
-mongodb.initDb((err, mongodb) => {
-    if (err) {
-        console.log(err);
-    } else {
-        app.listen(port);
-        console.log(`Now listening on port ${port}`);
-    }
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
 });
-
